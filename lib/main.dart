d408f19782e82b4024746c50251f77fe971026a3
achyutpratham77 <achyutpratham77@gmail.com>
2023-03-03 11:53:30 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dragablescrollablesheet/curosoul.dart';
 import 'package:flutter/material.dart';
 
 import 'MyHomePage.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const HomeScreen(),
     );
   }
 }
